df709ff667c0fdba143433777e428e90322d8a58
Guillaume Diaz <guillaume@qin-diaz.com>
2019-07-30 23:12:36 +0200

    GPG config (start)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,16 @@
+/**
+ * Offline reverse geocoding module
+ * @author Guillaume Diaz - fork (2019/07), version 1.0.0
+ * @author Daniel Glasson - original version 0.1
+ * @provides eu.daxiongmao.geocode offline reverse geocoding based on GeoCode database
+ * @uses java.base core java features
+ * @since v1.0.0
+ */
 module eu.daxiongmao.geocode {
+    // check required dependencies and modules with jdeps
+    // jdeps verbose jdkinternals ./target/offline-reverse-geocoding-1.0.0-SNAPSHOT.jar
+    requires java.base;
+
+    // class, packages and enum that are for the public
     exports eu.daxiongmao.geocode;
 }
